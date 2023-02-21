ad50f4751f9f40fa764d7d6f5e0c86fe89f6c994
Thiruvikraman J <109100403+Thiruvikraman1998@users.noreply.github.com>
2023-02-21 05:48:01 +0000

    Edited few lines

--- json.dart
+++ json.dart
@@ -1,36 +1,68 @@
 import 'dart:convert';
 
-class Student {
+// class Student {
+//   String? name;
+//   int? age;
+
+//   Student(this.name, this.age);
+
+//   Student.fromJson(Map<String, dynamic> json) {
+//     name = json["name"];
+//     age = json["age"];
+//   }
+
+//     Student.fromJson2(String jsonString){
+//         Map<String, dynamic> json = jsonDecode(jsonString);
+//         name = json['name'];
+//         age = json['age'];
+//     }
+//     // gets the argument from the s2 object and decodes the json map.
+// }
+
+// void main() {
+//   Map<String, dynamic> studentsMap = {"name": "Raj", "age": 25};
+
+//     //var parsedJson = jsonDecode(studentsMap);
+
+//     Student s = Student.fromJson(studentsMap);
+//   print("The student name is ${s.name} and age is ${s.age}");
+
+//     // String studentMap2 = '{"name": "Ram", "age": 25}';
+
+//     // Student s2 = Student.fromJson2(studentMap2);
+//     // print("The student name is ${s2.name} and age is ${s2.age}");
+// }
+
+//The above program has 2 examples.
+
+class Computer {
   String? name;
-  int? age;
+  double? price;
 
-  Student(this.name, this.age);
+  Computer(this.name, this.price);
 
-  Student.fromJson(Map<String, dynamic> json) {
+  Computer.fromJson(Map<String, dynamic> json) {
     name = json["name"];
-    age = json["age"];
+    price = json["price"];
   }
-
-    Student.fromJson2(String jsonString){
-        Map<String, dynamic> json = jsonDecode(jsonString);
-        name = json['name'];
-        age = json['age'];
+    Computer.fromJson2(String computerslist2){
+        Map<String, dynamic> json = jsonDecode(computerslist2);
+        name = json["name"];
+        price = json['price'];
     }
-    // gets the argument from the s2 object and decodes the json map.
 }
 
 void main() {
-  String studentsMap = '{"name": "Raj", "age": 25}';
-  
-    var parsedJson = jsonDecode(studentsMap);
-  
-    Student s = Student.fromJson(parsedJson);
-  print("The student name is ${s.name} and age is ${s.age}");
+  String computerList = '{"name": "Dell", "price": 2500.0}';
 
-    String studentMap2 = '{"name": "Ram", "age": 25}';
+  var parsedJson = jsonDecode(computerList);
 
-    Student s2 = Student.fromJson2(studentMap2);
-    print("The student name is ${s2.name} and age is ${s2.age}");
+  Computer c1 = Computer.fromJson(parsedJson);
+
+  print("The price of ${c1.name} is ${c1.price}");
+
+    String computerList2 = '{"name": "Lenovo", "price": 2500.25}';
+
+    Computer c2 = Computer.fromJson2(computerList2);
+    print("The price of ${c2.name} is ${c2.price}");
 }
-
-//The above program has 2 examples.
