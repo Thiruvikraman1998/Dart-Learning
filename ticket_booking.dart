f35a1a3dd9da209db2c1383c15dfddd15b77a624
Thiruvikraman J <109100403+Thiruvikraman1998@users.noreply.github.com>
2023-02-23 18:41:50 +0000

    Committed changes.

--- ticket_booking.dart
+++ ticket_booking.dart
@@ -6,65 +6,54 @@ class Passenger {
   String? name;
   int? age;
   String? gender;
-  dynamic dateOfJourney;
 
-  // Passenger(String name, int age, String gender, dynamic dateOfJourney){
-  // this.name = name;
-  //     this.age = age;
-  //     this.gender = gender;
-  //     this.dateOfJourney = dateOfJourney;
-  Passenger();
+  Passenger({required this.name, required this.age, required this.gender});
 
   void ticket() {
-    // stdout.write("Please enter passenger Name: ");
-    // String passName = stdin.readLineSync()!;
-    // this.name = passName;
-    // stdout.write("Please enter passenger Age: ");
-    // int passAge = int.parse(stdin.readLineSync()!);
-    // this.age = passAge;
-    // stdout.write("Please enter passenger Gender: ");
-    // String passGender = stdin.readLineSync()!;
-    // this.gender = passGender;
-    // stdout.write("Please enter travel Date: ");
-    // dynamic travelDate = stdin.readLineSync().toString();
-    // this.dateOfJourney = travelDate;
     stdout.write(
-        "\n Passenger name: $name \n Passenger age: $age \n Passenger Gender: $gender \n Travel Date: $dateOfJourney \n");
+        "\n Passenger name: $name \n Passenger age: $age \n Passenger Gender: $gender \n");
     stdout.write("Your tickets booked successfully");
   }
 }
 
 void main() {
-  // stdout.write("Please enter passenger Name: ");
-  // String name  = stdin.readLineSync()!;
+  List<Passenger>? ticketList;
+  String? passName;
+  int? passAge;
+  String? passGender;
 
-  // stdout.write("Please enter passenger Age: ");
-  // int age = int.parse(stdin.readLineSync()!);
-
-  // stdout.write("Please enter passenger Gender: ");
-  // String gender = stdin.readLineSync()!;
-
-  // stdout.write("Please enter travel Date: ");
-  // String traveldate = stdin.readLineSync()!;
   stdout.write("No.of tickets: \n");
   int noOfTickets = int.parse(stdin.readLineSync()!);
-  Passenger tickets = Passenger();
+
   stdout.write("Please enter travel Date: ");
-  tickets.dateOfJourney = stdin.readLineSync();
-  // p1.name = name;
-  // p1.age = age;
-  // p1.gender = gender;
-  // p1.dateOfJourney = traveldate;
+  String? dateOfJourney = stdin.readLineSync();
+
+  Passenger tickets =
+      Passenger(name: passName, age: passAge, gender: passGender);
+
   for (int i = 1; i <= noOfTickets; i++) {
-    tickets.i = Passenger();
     stdout.write("Please enter passenger Name: ");
     tickets.name = stdin.readLineSync();
+    passName = tickets.name;
 
     stdout.write("Please enter passenger Age: ");
     tickets.age = int.parse(stdin.readLineSync()!);
+    passAge = tickets.age;
 
     stdout.write("Please enter passenger Gender: ");
-    tickets.gender = stdin.readLineSync()!;
+    tickets.gender = stdin.readLineSync();
+    passGender = tickets.gender;
+
+    ticketList!
+        .add(Passenger(name: passName, age: passAge, gender: passGender));
+  }
+
+  print("Your travel date is: $dateOfJourney");
+
+  tickets.ticket();
+
+  for (Passenger tickets in ticketList!) {
+    print(tickets);
   }
   tickets.ticket();
 }
