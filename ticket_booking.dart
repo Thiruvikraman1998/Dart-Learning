8dad969d66498385975415830d6af40aaa79bc86
Thiruvikraman J <109100403+Thiruvikraman1998@users.noreply.github.com>
2023-02-26 06:49:44 +0000

    Added changes

--- ticket_booking.dart
+++ ticket_booking.dart
@@ -21,30 +21,47 @@ void main() {
   String? passName;
   int? passAge;
   String? passGender;
-
-  stdout.write("No.of tickets: \n");
-  int noOfTickets = int.parse(stdin.readLineSync()!);
+  int fare = 200;
+  int noOfTickets = 0;
 
   stdout.write("Please enter travel Date: ");
   String? dateOfJourney = stdin.readLineSync();
 
+  print("Add Passenger?(y/n)");
+  String? addPassenger = stdin.readLineSync();
   Passenger tickets = Passenger(passName, passAge, passGender);
 
-  for (int i = 1; i <= noOfTickets; i++) {
-    stdout.write("Please enter passenger Name: ");
-    tickets.name = stdin.readLineSync();
-    passName = tickets.name;
+  if (addPassenger == "y") {
+    noOfTickets = noOfTickets + 1;
 
-    stdout.write("Please enter passenger Age: ");
-    tickets.age = int.parse(stdin.readLineSync()!);
-    passAge = tickets.age;
+    for (int i = noOfTickets; i <= noOfTickets; i++) {
+      stdout.write("Please enter passenger Name: ");
+      tickets.name = stdin.readLineSync();
+      passName = tickets.name;
 
-    stdout.write("Please enter passenger Gender: ");
-    tickets.gender = stdin.readLineSync();
-    passGender = tickets.gender;
+      stdout.write("Please enter passenger Age: ");
+      tickets.age = int.parse(stdin.readLineSync()!);
+      passAge = tickets.age;
 
-    ticketList.add(Passenger(passName, passAge, passGender));
+      stdout.write("Please enter passenger Gender: ");
+      tickets.gender = stdin.readLineSync();
+      passGender = tickets.gender;
+
+      ticketList.add(Passenger(passName, passAge, passGender));
+
+      print("Do you want to add more passengers:(y/n)");
+      String? addMorePass = stdin.readLineSync();
+      if (addMorePass == "y") {
+        noOfTickets += 1;
+      } else {
+        break;
+      }
+    }
+  } else {
+    print("No passengers added");
   }
+
+  num totalFare = fare * noOfTickets;
 
   print("\n \n Your travel date is: $dateOfJourney");
 
@@ -62,4 +79,5 @@ void main() {
 
   viewTicket();
   tickets.ticket();
+  print("\nTotal fare is: $totalFare");
 }
